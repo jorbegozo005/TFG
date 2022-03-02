05d482b6f8e94bf3128c0fb121573c168efd72f8
jorbegozo005 <56362215+jorbegozo005@users.noreply.github.com>
2022-03-02 11:53:05 +0100

    1 thread

--- main.cu
+++ main.cu
@@ -21,61 +21,61 @@ extern int gmax;*/
 __global__
 void NbodyODE2Tpgpuaux(int gmax, int nplanetas, int xyz, int tamano, int dimensiones, int N, float *Gm, float *aux, float *q, float *ddq) {
 
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    //int j = blockIdx.x * blockDim.x + threadIdx.x;
+    int i, j, k, m;
+    float Gmi, Gmj, berretzailea, lag;
 
-    float Gmi;
-    printf("Antes del if %d\n",i);
+    i = threadIdx.x;
 
-    if (i < nplanetas) {
-        printf("Entra %d\n",i);
+    //printf("Nbody: %d\n", i); OPENMP
+
+    if (i < nplanetas && i < 1) {
         Gmi = Gm[i];
-        for (int j=i+1; j<nplanetas; j++) {
-            float Gmj = Gm[j];
+        for (j=i+1; j<nplanetas; j++) {
+            Gmj = Gm[j];
         
-            for (int k=0; k<dimensiones; k++) {
+            for (k=0; k<dimensiones; k++) {
                 aux[k*N+0] = q[k*tamano+0+i*xyz] - q[k*tamano+0+j*xyz];
                 aux[k*N+1] = q[k*tamano+1+i*xyz] - q[k*tamano+1+j*xyz];
                 aux[k*N+2] = q[k*tamano+2+i*xyz] - q[k*tamano+2+j*xyz];
             }
-            for (int k=0; k<dimensiones; k++) {
+            for (k=0; k<dimensiones; k++) {
                 aux[k*N+3] = 0.0;
                 aux[k*N+4] = 0.0;
                 aux[k*N+5] = 0.0;
-                for (int m=0; m<=k; m++) {
+                for (m=0; m<=k; m++) {
                     aux[k*N+3] += aux[m*N+0] * aux[(k-m)*N+0];
                     aux[k*N+4] += aux[m*N+1] * aux[(k-m)*N+1];
                     aux[k*N+5] += aux[m*N+2] * aux[(k-m)*N+2];
                 }
             }
 
-            for (int k=0; k<dimensiones; k++) {
+            for (k=0; k<dimensiones; k++) {
                 aux[k*N+6] = aux[k*N+3] + aux[k*N+4] + aux[k*N+5];
                 aux[k*N+7] = 0.0;
             }
 
-            float berretzailea = -3.0/2.0;
+            berretzailea = -3.0/2.0;
             aux[7] = pow(aux[6],berretzailea);
-            for (int k=1; k<=dimensiones-1; k++) {
-                float lag = 0.0;
-                for (int m=0; m<=k; m++) {
+            for (k=1; k<=dimensiones-1; k++) {
+                lag = 0.0;
+                for (m=0; m<=k; m++) {
                     lag = lag + (berretzailea*(k-m)-m)*aux[(k-1-m+1)*N+6]*aux[m*N+7];
                 }
                 aux[k*N+7]=lag/(k*aux[6]);
             }            
 
-            for (int k=0; k<dimensiones; k++) {
+            for (k=0; k<dimensiones; k++) {
                 aux[k*N+3]=0.0;
                 aux[k*N+4]=0.0;
                 aux[k*N+5]=0.0;
-                for (int m=1; m<=k+1; m++) {
+                for (m=1; m<=k+1; m++) {
                     aux[k*N+3] +=  aux[(m-1)*N+7]*aux[(k-m+1)*N];
                     aux[k*N+4] +=  aux[(m-1)*N+7]*aux[(k-m+1)*N+1];
                     aux[k*N+5] +=  aux[(m-1)*N+7]*aux[(k-m+1)*N+2];
                 }
             }
 
-            for (int k=0; k<dimensiones; k++) { 
+            for (k=0; k<dimensiones; k++) { 
                 ddq[k*tamano+0+i*xyz] -= Gmj*aux[k*N+3];
                 ddq[k*tamano+0+j*xyz] += Gmi*aux[k*N+3];
                 ddq[k*tamano+1+i*xyz] -= Gmj*aux[k*N+4];
@@ -88,18 +88,109 @@ void NbodyODE2Tpgpuaux(int gmax, int nplanetas, int xyz, int tamano, int dimensi
 }
 
 __global__
-void NuevoGradogpu(nplanetas, xyz, tamano, zat, zat2, k, gmax, nu, nddu) {
+void NuevoGradogpu(int gmax, int nplanetas, int xyz, int tamano, int zat, int zat2, int k, float *nu, float *nddu) {
     int body, koor;
 
     body = blockIdx.x;
     koor = threadIdx.x;
 
-    if (body < nplanetas && koor < xyz) {
+    //printf("Grado: %d, %d\n", body, koor);
+
+    if (body < nplanetas && koor < xyz && koor<1) {
         nu[(k-1)*tamano+koor*nplanetas+body] =nddu[(k-3)*tamano+koor*nplanetas+body]/zat;
         if (k < gmax)
             nu[k*tamano+koor*nplanetas+body] =nddu[(k-2)*tamano+koor*nplanetas+body]/zat2;
     }
 
+}
+
+__global__
+void evaluatetaylorvgpu(int nplanetas, int xyz, int tamano, int dimensiones, float *nu, float h) {
+
+    int body, k;
+    float x, y, z, dx, dy, dz, ukx, uky, ukz;
+
+    body = threadIdx.x;
+
+    //printf("Eval: %d\n", body);
+
+    if (body < nplanetas && body<1) {
+
+    //for (body=0; body<nplanetas; body++) {//body in 1:N
+        x = nu[(dimensiones-1)*tamano+0+body*xyz]*h;
+        nu[(dimensiones-1)*tamano+0+body*xyz]=0.0;
+        y= nu[(dimensiones-1)*tamano+1+body*xyz]*h;
+        nu[(dimensiones-1)*tamano+1+body*xyz]=0.0;
+        z= nu[(dimensiones-1)*tamano+2+body*xyz]*h;
+        nu[(dimensiones-1)*tamano+2+body*xyz]=0.0;
+        dx= (dimensiones-1)*x;
+        dy= (dimensiones-1)*y;
+        dz= (dimensiones-1)*z;
+        for (k=dimensiones-1; k>1; k--) {//k in n-1:-1:3 // azken bi terminoak era berezian tratatuko ditut,
+                          // izan ere, u'(t) espresioak ez baitauka t biderkatzen.
+            ukx= nu[k*tamano+0+body*xyz];
+            nu[k*tamano+0+body*xyz]=0.0;
+            uky= nu[k*tamano+1+body*xyz];
+            nu[k*tamano+1+body*xyz]=0.0;
+            ukz= nu[k*tamano+2+body*xyz];
+            nu[k*tamano+2+body*xyz]=0.0;
+            x+=ukx;
+            x*=h;
+            y+=uky;
+            y*=h;
+            z+=ukz;
+            z*=h;
+            dx+= (k-1)*ukx;
+            dx*=h;
+            dy+= (k-1)*uky;
+            dy*=h;
+            dz+= (k-1)*ukz;
+            dz*=h;
+        }
+        // orain bigarren terminoari dagokiona: 
+        // u(t) kasuan orain artekoa honi gehitu eta dena * h
+        // u'(t) kasuan  u_2 balioari gehitu orain artekoa eta laga u_2 berri bezala
+        ukx= nu[1*tamano+0+body*xyz];
+        uky= nu[1*tamano+1+body*xyz];
+        ukz= nu[1*tamano+2+body*xyz];
+        x+=ukx;
+        x*=h;
+        y+=uky;
+        y*=h;
+        z+=ukz;
+        z*=h;
+        nu[1*tamano+0+body*xyz] += dx;
+        nu[1*tamano+1+body*xyz] += dy;
+        nu[1*tamano+2+body*xyz] += dz;
+        // Eta bukatzeko, lehenengo terminoari dagokiona: hau u(t) espresioan bakarrik dago,
+        // eta u_1 koefizienteari orain artekoa gehitu behar diot
+        nu[/*0*tamano+0+*/body*xyz] += x;
+        nu[1+body*xyz] += y;
+        nu[2+body*xyz] += z;
+    }
+}
+
+__global__
+void initCeros(int tamano, int gmax, float *ddu) {
+
+    int i;
+
+    i = threadIdx.x;
+
+    if (i<tamano*gmax && i<1) {
+        ddu[i] = 0.0;
+    }
+
+}
+
+void inicializarGlobales() {
+    nplanetas = 5;
+    xyz = 3;
+    tamano = nplanetas*xyz;
+    dimensiones = 2;
+    N = 8;
+    gmax = 8;
+    //gpu = false;
 }
 
 void NbodyODE2Tpgpu(float *ddq, float *q, float *Gm, float *aux) {
@@ -125,7 +216,7 @@ void NbodyODE2Tpgpu(float *ddq, float *q, float *Gm, float *aux) {
 
     printf("Antes\n");
     
-    NbodyODE2Tpgpuaux<<<2, nplanetas>>>(gmax, nplanetas, xyz, tamano, dimensiones, N, d_Gm, d_aux, d_q, d_ddq);
+    NbodyODE2Tpgpuaux<<<1, nplanetas>>>(gmax, nplanetas, xyz, tamano, dimensiones, N, d_Gm, d_aux, d_q, d_ddq);
 
     cudaDeviceSynchronize();
 
@@ -135,16 +226,11 @@ void NbodyODE2Tpgpu(float *ddq, float *q, float *Gm, float *aux) {
     cudaMemcpy(aux, d_aux, N*xyz*sizeof(float), cudaMemcpyDeviceToHost);
     cudaMemcpy(q, d_q, tamano*gmax*sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(ddq, d_ddq, tamano*gmax*sizeof(float), cudaMemcpyDeviceToHost);
-}
 
-void inicializarGlobales() {
-    nplanetas = 5;
-    xyz = 3;
-    tamano = nplanetas*xyz;
-    dimensiones = 2;
-    N = 8;
-    gmax = 8;
-    gpu = true;
+    cudaFree(d_Gm);
+    cudaFree(d_aux);
+    cudaFree(d_q);
+    cudaFree(d_ddq);
 }
 
 void NbodyODE2Tp(float *ddq, float *q, float *Gm, float *aux) {
@@ -214,32 +300,36 @@ void NbodyODE2Tp(float *ddq, float *q, float *Gm, float *aux) {
 }
 
 void TaylorLortuP(float *nu, float *nddu, float *gm, float *aux) {
-    int gradua = gmax-1;
 
-    for (int i = 0; i<gradua/2; i++) {
-        if (gpu) {
+    int gradua, i, k, body, koor;
+    float zat, zat2;
+
+    gradua = gmax-1;
+
+    for (i = 0; i<gradua/2; i++) {
+        /*if (gpu) {
             NbodyODE2Tpgpu(nddu,nu,gm,aux);
-        } else {
-            NbodyODE2Tp(nddu,nu,gm,aux);
-        }
+        } else {*/
+        NbodyODE2Tp(nddu,nu,gm,aux);
+        //}
         
-        int k = 2*(i+1)+1;
+        k = 2*(i+1)+1;
 
         if (k < gmax) {   
-            float zat=(k-1)*(k-2);
-            float zat2=k*(k-1);
+            zat=(k-1)*(k-2);
+            zat2=k*(k-1);
             dimensiones+=2;
-            for (int body=0; body<nplanetas; body++) {
-                for (int koor=0; koor<xyz; koor++) {
+            for (body=0; body<nplanetas; body++) {
+                for (koor=0; koor<xyz; koor++) {
                     nu[(k-1)*tamano+koor*nplanetas+body] =nddu[(k-3)*tamano+koor*nplanetas+body]/zat;
                     nu[k*tamano+koor*nplanetas+body] =nddu[(k-2)*tamano+koor*nplanetas+body]/zat2;
                 }
             }
         } else {
-            float zat=(k-1)*(k-2);
+            zat=(k-1)*(k-2);
             dimensiones++;
-            for (int body=0; body<nplanetas; body++) {
-                for (int koor=0; koor<xyz; koor++) {
+            for (body=0; body<nplanetas; body++) {
+                for (koor=0; koor<xyz; koor++) {
                     nu[(k-1)*tamano+koor*nplanetas+body] = nddu[(k-3)*tamano+koor*nplanetas+body]/zat;
                 }
             }
@@ -251,8 +341,9 @@ void TaylorLortuP(float *nu, float *nddu, float *gm, float *aux) {
 void evaluatetaylorv(float *nu, float h) {
 
     float x, y, z, dx, dy, dz, ukx, uky, ukz;
+    int body, k;
 
-    for (int body=0; body<nplanetas; body++) {//body in 1:N
+    for (body=0; body<nplanetas; body++) {//body in 1:N
         x = nu[(dimensiones-1)*tamano+0+body*xyz]*h;
         nu[(dimensiones-1)*tamano+0+body*xyz]=0.0;
         y= nu[(dimensiones-1)*tamano+1+body*xyz]*h;
@@ -262,7 +353,7 @@ void evaluatetaylorv(float *nu, float h) {
         dx= (dimensiones-1)*x;
         dy= (dimensiones-1)*y;
         dz= (dimensiones-1)*z;
-        for (int k=dimensiones-1; k>1; k--) {//k in n-1:-1:3 // azken bi terminoak era berezian tratatuko ditut,
+        for (k=dimensiones-1; k>1; k--) {//k in n-1:-1:3 // azken bi terminoak era berezian tratatuko ditut,
                           // izan ere, u'(t) espresioak ez baitauka t biderkatzen.
             ukx= nu[k*tamano+0+body*xyz];
             nu[k*tamano+0+body*xyz]=0.0;
@@ -306,14 +397,10 @@ void evaluatetaylorv(float *nu, float h) {
     }
 }
 
-void TaylorStepPgpu(float *u, float *ddu, float *gm, float *aux, float h) {
+void TaylorStepPgpu(float *u, float *ddu, float *gm, float *aux, float h, int ukop) {
 
     float *d_Gm, *d_aux, *d_q, *d_ddq, zat, zat2;
-    int gradua, i, k, body, koor;
-
-    for (i=0; i<tamano*gmax; i++) {
-        ddu[i] = 0.0;
-    }
+    int gradua, i, j, k;
 
     cudaMalloc(&d_Gm, nplanetas*sizeof(float)); 
     cudaMalloc(&d_aux, N*xyz*sizeof(float));
@@ -324,33 +411,50 @@ void TaylorStepPgpu(float *u, float *ddu, float *gm, float *aux, float h) {
     cudaMemcpy(d_aux, aux, N*xyz*sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_q, u, tamano*gmax*sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_ddq, ddu, tamano*gmax*sizeof(float), cudaMemcpyHostToDevice);
+
+    for (j=1; j<=ukop; j++) {
     
-    gradua = gmax-1;
+        gradua = gmax-1;
 
-    for (i = 0; i<gradua/2; i++) {
-
-        NbodyODE2Tpgpuaux<<<1, nplanetas>>>(gmax, nplanetas, xyz, tamano, dimensiones, N, d_Gm, d_aux, d_q, d_ddq);
+        initCeros<<<1,gmax*tamano>>>(tamano, gmax, d_ddq);
         cudaDeviceSynchronize();
 
-        k = 2*(i+1)+1;
+        for (i = 0; i<gradua/2; i++) {
 
-        zat=(k-1)*(k-2);
-        zat2=k*(k-1);
+            NbodyODE2Tpgpuaux<<<1, nplanetas>>>(gmax, nplanetas, xyz, tamano, dimensiones, N, d_Gm, d_aux, d_q, d_ddq);
+            cudaDeviceSynchronize();
 
-        if (k < gmax) {   
-            dimensiones+=2;
-        } else {
-            dimensiones++;
+            k = 2*(i+1)+1;
+
+            zat=(k-1)*(k-2);
+            zat2=k*(k-1);
+
+            if (k < gmax) {   
+                dimensiones+=2;
+            } else {
+                dimensiones++;
+            }
+
+            NuevoGradogpu<<<nplanetas,xyz>>>(gmax, nplanetas, xyz, tamano, zat, zat2, k, d_q, d_ddq);
+            cudaDeviceSynchronize();
+    
         }
 
-        // paralelizado 2
-        NuevoGradogpu<<<nplanetas,xyz>>>(nplanetas, xyz, tamano, zat, zat2, k, gmax, d_q, d_ddq);
+        evaluatetaylorvgpu<<<1,nplanetas>>>(nplanetas, xyz, tamano, dimensiones, d_q, h);
         cudaDeviceSynchronize();
- 
+
+        dimensiones = 2;
     }
 
-    //evaluatetaylorv(); //paralelizado
+    cudaMemcpy(gm, d_Gm, nplanetas*sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(aux, d_aux, N*xyz*sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(u, d_q, tamano*gmax*sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(ddu, d_ddq, tamano*gmax*sizeof(float), cudaMemcpyDeviceToHost);
 
+    /*cudaFree(d_Gm);
+    cudaFree(d_aux);
+    cudaFree(d_q);
+    cudaFree(d_ddq);*/
 }
 
 void TaylorStepP(float *u, float *ddu, float *gm, float *aux, float h) {
@@ -361,53 +465,71 @@ void TaylorStepP(float *u, float *ddu, float *gm, float *aux, float h) {
 
 void IntegrateTaylorP(float *u, int t0, int tf, float h, float *gm) {
 
-    float *ddu;
+    float *ddu, *aux;
+    int i, j, ukop;
+    clock_t begin, end;
+    double time_spent;
+
     ddu = (float *) malloc(tamano*gmax*sizeof(float));
     //cudaMallocManaged(&ddu, tamano*gmax*sizeof(float));
     for (int i=0; i<tamano*gmax; i++) {
         ddu[i] = 0.0;
     }
 
-    float *aux;
     aux = (float *) malloc(N*xyz*sizeof(float));
     //cudaMallocManaged(&aux, N*xyz*sizeof(float));
-    for (int i=0; i<N*xyz; i++) {
+    for (i=0; i<N*xyz; i++) {
         aux[i] = 0.0;
     }
 
     printf("Inicializada u:\n");
-    for (int i=0; i<gmax; i++){
-        for (int j=0; j<nplanetas; j++) {
+    for (i=0; i<gmax; i++){
+        for (j=0; j<nplanetas; j++) {
             printf("%g %g %g;   ", u[i*tamano+j*xyz], u[i*tamano+j*xyz+1], u[i*tamano+j*xyz+2]);
         }
         printf("\n");
     }
 
-    int ukop = (tf-t0)/h;
-    printf("%d\n", ukop);
-    ukop = 2;
-    clock_t begin = clock();
-    for (int i=1; i<=ukop; i++) {
-        if (gpu) {
-            TaylorStepP(u, ddu, gm, aux, h);
-        } else {
-            TaylorStepP(u, ddu, gm, aux, h);
+    ukop = (tf-t0)/h;
+    ukop = 10;
+    printf("Pasos: %d\n", ukop);
+
+    begin = clock();
+    
+    //TaylorStepPgpu(u, ddu, gm, aux, h, ukop);
+
+    end = clock();
+
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Tiempo gpu: %f\n", time_spent);
+
+    /*printf("FINAL gpu\n");
+    for (i=0; i<gmax; i++){
+        for (j=0; j<nplanetas; j++) {
+            printf("%g %g %g;   ", u[i*tamano+j*xyz], u[i*tamano+j*xyz+1], u[i*tamano+j*xyz+2]);
         }
+        printf("\n");
+    }*/
+
+    begin = clock();
+    
+    for (i=1; i<=ukop; i++) {
+        TaylorStepP(u, ddu, gm, aux, h);
         dimensiones = 2;
     }
-    clock_t end = clock();
+        
+    end = clock();
 
-    printf("FINAL");
-    for (int i=0; i<gmax; i++){
-        for (int j=0; j<nplanetas; j++) {
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Tiempo cpu: %f\n", time_spent);
+
+    /*printf("FINAL cpu\n");
+    for (i=0; i<gmax; i++){
+        for (j=0; j<nplanetas; j++) {
             printf("%g %g %g;   ", u[i*tamano+j*xyz], u[i*tamano+j*xyz+1], u[i*tamano+j*xyz+2]);
         }
         printf("\n");
-    }
-
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-
-    printf("Tiempo: %f\n", time_spent);
+    }*/
 
     free(ddu);
     free(aux);
@@ -419,9 +541,11 @@ void initialInnerPlanets(float *Gm, float *u, int tamano) {
 Order of planets:
 Sun, Mercury, Venus, Earth, Mars, Moon
 */
+    float GmMoon, GmEarth, sumGm, resultadoqx, resultadoqy, resultadoqz, qxbar, qybar, qzbar, resultadovx, resultadovy, resultadovz, vxbar, vybar, vzbar;
+    int i;
 
-    float GmMoon = 0.109318945074237400e-10;
-    float GmEarth = 0.888769244512563400e-9;
+    GmMoon = 0.109318945074237400e-10;
+    GmEarth = 0.888769244512563400e-9;
 
     Gm[0] = 0.295912208285591100e-3;
     Gm[1] = 0.491248045036476000e-10;
@@ -443,53 +567,53 @@ Sun, Mercury, Venus, Earth, Mars, Moon
       0.01448165305704756448, 0.00024246307683646861, -0.00028152072792433877  // Mars
     };
 
-    float sumGm = 0.0;
-    for (int i=0; i<5; i++) {
+    sumGm = 0.0;
+    for (i=0; i<5; i++) {
        sumGm = sumGm + Gm[i];
     }
 
-    float resultadoqx = 0.0;
-    float resultadoqy = 0.0;
-    float resultadoqz = 0.0;
-    for (int i=0; i<5; i++) {
+    resultadoqx = 0.0;
+    resultadoqy = 0.0;
+    resultadoqz = 0.0;
+    for (i=0; i<5; i++) {
        resultadoqx =  resultadoqx + (Gm[i]*q0[i*3]);
        resultadoqy =  resultadoqy + (Gm[i]*q0[i*3+1]);
        resultadoqz =  resultadoqz + (Gm[i]*q0[i*3+2]);
     }
-    float qxbar = resultadoqx/sumGm;
-    float qybar = resultadoqy/sumGm;
-    float qzbar = resultadoqz/sumGm;
+    qxbar = resultadoqx/sumGm;
+    qybar = resultadoqy/sumGm;
+    qzbar = resultadoqz/sumGm;
 
-    float resultadovx = 0.0;
-    float resultadovy = 0.0;
-    float resultadovz = 0.0;
-    for (int i=0; i<5; i++) {
+    resultadovx = 0.0;
+    resultadovy = 0.0;
+    resultadovz = 0.0;
+    for (i=0; i<5; i++) {
        resultadovx =  resultadovx + (Gm[i]*v[i*3]);
        resultadovy =  resultadovy + (Gm[i]*v[i*3+1]);
        resultadovz =  resultadovz + (Gm[i]*v[i*3+2]);
     }
-    float vxbar = resultadovx/sumGm;
-    float vybar = resultadovy/sumGm;
-    float vzbar = resultadovz/sumGm;
+    vxbar = resultadovx/sumGm;
+    vybar = resultadovy/sumGm;
+    vzbar = resultadovz/sumGm;
 
-    for (int i=0; i<5; i++) {
+    for (i=0; i<5; i++) {
        q0[i*3] = q0[i*3] - qxbar;
        q0[i*3+1] = q0[i*3+1] - qybar;
        q0[i*3+2] = q0[i*3+2] - qzbar;
     }
 
-    for (int i=0; i<5; i++) {
+    for (i=0; i<5; i++) {
        v[i*3] = v[i*3] - vxbar;
        v[i*3+1] = v[i*3+1] - vybar;
        v[i*3+2] = v[i*3+2] - vzbar;
     }
 
-    for (int i=0; i<tamano; i++) {
+    for (i=0; i<tamano; i++) {
         u[i] = q0[i];
         u[i+tamano] = v[i];
     }
 
-    for (int i=nplanetas*xyz*2; i<gmax*nplanetas*xyz; i++) {
+    for (i=nplanetas*xyz*2; i<gmax*nplanetas*xyz; i++) {
         u[i] = 0.0;
     }
 
@@ -498,21 +622,22 @@ Sun, Mercury, Venus, Earth, Mars, Moon
 
 int main() {
 
+    float *GM, *u, h;
+    int t0, tf;
+
     inicializarGlobales();
 
-    float *GM;
     GM = (float *) malloc(nplanetas*sizeof(float));
     //cudaMallocManaged(&GM, nplanetas*sizeof(float));
     
-    float *u;
     u = (float *) malloc(tamano*gmax*sizeof(float));
     //cudaMallocManaged(&u, tamano*gmax*sizeof(float));
 
     initialInnerPlanets(GM, u, tamano);
 
-    int t0 = 0;
-    int tf = 360;
-    float h = 0.01;
+    t0 = 0;
+    tf = 360;
+    h = 0.01;
 
     IntegrateTaylorP(u, t0, tf, h, GM);
 
